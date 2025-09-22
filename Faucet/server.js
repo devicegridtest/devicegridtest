32a87c846db4ae9f3d1208cca724467439b5609f
devicegridtest <antibot586@gmail.com>
2025-09-22 14:42:26 -0500

    Add files via upload

--- Faucet/server.js
+++ Faucet/server.js
@@ -12,6 +12,8 @@ app.use(cors({
     origin: [
         'null',
         'http://localhost:3000',
+        'http://127.0.0.1:5500',
+        'http://127.0.0.1:8080',
         'https://tudominio.com',
         'https://devicegridtest.org'
     ],
